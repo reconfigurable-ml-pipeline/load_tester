2b2c6fdbcdc0a97b5f9cdc125f61884999483dc6
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-10-12 00:13:25 +0000

    mlserver pipeliens completed

--- barazmoon/main.py
+++ barazmoon/main.py
@@ -4,26 +4,29 @@ import numpy as np
 from multiprocessing import Process, Value, active_children
 import asyncio
 from aiohttp import ClientSession
+from seldon_core.seldon_client import SeldonClient
 
 
 class BarAzmoon:
-    endpoint: str = None
-    # timeout: int = None  # timeout: seconds to wait for server to respond to request, ignore when timed out
-    http_method = "get"
     def __init__(self):
-        self.__workload = self.get_workload()
-        self.__success_counter = Value("i", 0)  # Actually, those who did not timed out
-        self.__counter = 0
+        self.endpoint: str = None
+        # timeout: int = None  # timeout: seconds to wait for server to respond to request, ignore when timed out
+        self.http_method = "get"
+        self._workload = self.get_workload()
+        self._success_counter = Value("i", 0)  # Actually, those who did not timed out
+        self._counter = 0
     
     def get_workload(self) -> List[int]:
         raise NotImplementedError
     
     def start(self):
         total_seconds = 0
-        for rate in self.__workload:
+        for rate in self._workload:
             total_seconds += 1
-            self.__counter += rate
-            generator_process = Process(target=self.target_process, args=(rate, self.__success_counter))
+            self._counter += rate
+            generator_process = Process(
+                target=self.target_process, args=(
+                    rate, self._success_counter))
             generator_process.daemon = True
             generator_process.start()
             active_children()
@@ -34,42 +37,89 @@ class BarAzmoon:
         
         print(f"total seconds: {total_seconds}")
 
-        return (self.__counter, self.__counter - self.__success_counter.value)
+        return (self._counter, self._counter - self._success_counter.value)
 
-    @classmethod
-    def target_process(cls, count, success_counter):
-        asyncio.run(cls.generate_load_for_second(count, success_counter))
+    def target_process(self, count, success_counter):
+        asyncio.run(self.generate_load_for_second(count, success_counter))
 
-    @classmethod
-    async def generate_load_for_second(cls, count, success_counter):
+    async def generate_load_for_second(self, count, success_counter):
         async with ClientSession() as session:
-            delays = np.cumsum(np.random.exponential(1 / (count * 1.5), count))
+            delays = np.cumsum(
+                np.random.exponential(1 / (count * 1.5), count))
             tasks = []
             for i in range(count):
-                task = asyncio.ensure_future(cls.predict(delays[i], session, success_counter))
+                task = asyncio.ensure_future(
+                    self.predict(delays[i], session, success_counter))
                 tasks.append(task)
             await asyncio.gather(*tasks)
     
-    @classmethod
-    async def predict(cls, delay, session, success_counter):
+    async def predict(self, delay, session, success_counter):
         await asyncio.sleep(delay)
-        data_id, data = cls.get_request_data()
-        async with getattr(session, cls.http_method)(cls.endpoint, data=data) as response:
+        data_id, data = self.get_request_data()
+        async with getattr(
+            session, self.http_method)(self.endpoint, data=data) as response:
             response = await response.json()
             if "error" not in response.keys():
                 loop = asyncio.get_event_loop()
-                await loop.run_in_executor(None, cls.increment_value, success_counter)
-            cls.process_response(data_id, response)
+                await loop.run_in_executor(
+                    None, self.increment_value, success_counter)
+            self.process_response(data_id, response)
             return 1
     
-    @staticmethod
     def increment_value(value):
         value.value += 1
 
-    @classmethod
-    def get_request_data(cls) -> Tuple[str, str]:
+    def get_request_data(self) -> Tuple[str, str]:
         return None, None
     
-    @classmethod
-    def process_response(cls, data_id: str, response: dict):
+    def process_response(self, data_id: str, response: dict):
         pass
+
+class SeldonBarAzmoon(BarAzmoon):
+    def set_seldon_params(
+        self, gateway_endpoint, deployment_name, namespace):
+        self.sc = SeldonClient(
+            gateway_endpoint=gateway_endpoint,
+            gateway="istio",
+            transport="rest",
+            deployment_name=deployment_name,
+            namespace=namespace)
+
+    async def predict(self, delay, session, success_counter):
+        await asyncio.sleep(delay)
+        data_id, data = self.get_request_data()
+        async with self.sc.predict(inputs = data) as response:
+            if response.success:
+                json_data_timer = response.response['jsonData']['time']
+                return 1
+
+class MLServerBarAzmoon(BarAzmoon):
+    async def predict(self, delay, session, success_counter):
+        await asyncio.sleep(delay)
+        data = self.get_request_data()
+
+        async with getattr(
+            session, self.http_method)(
+                self.endpoint, json=data) as response:
+            response = await response.json()
+            print(response)
+            if "error" not in response.keys():
+                loop = asyncio.get_event_loop()
+                await loop.run_in_executor(
+                    None, self.increment_value, success_counter)
+            return 1
+    
+    def get_request_data(self) -> Tuple[str, str]:
+        input_ins = {
+            "name": "parameters-np",
+            "datatype": "FP32",
+            "shape": [2, 1],
+            "data": [1, 2],
+            "parameters": {
+                "content_type": "np"
+                }
+            }
+        payload = {
+            "inputs": [input_ins]
+        }
+        return payload
