eef7e084b9154959b6fb2cebd921439487ad5fbb
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2022-10-13 20:53:52 +0000

    added mlserver

--- barazmoon/__init__.py
+++ barazmoon/__init__.py
@@ -1,3 +1,2 @@
 from barazmoon.main import BarAzmoon
 from barazmoon.main import MLServerBarAzmoon
-from barazmoon.main import SeldonBarAzmoon
--- barazmoon/main.py
+++ barazmoon/main.py
@@ -1,32 +1,26 @@
 import time
-from typing import Iterable, List, Tuple
+from typing import List, Tuple
 import numpy as np
-from multiprocessing import Process, Value, active_children
+from multiprocessing import Process, active_children
 import asyncio
 from aiohttp import ClientSession
-from seldon_core.seldon_client import SeldonClient
+import json
 
 
 class BarAzmoon:
-    def __init__(self, endpoint, http_method):
-        self.endpoint: str = endpoint
-        # timeout: int = None  # timeout: seconds to wait for server to respond to request, ignore when timed out
+    def __init__(self, *, workload: List[int], endpoint: str, http_method = "get", **kwargs):
+        self.endpoint = endpoint
         self.http_method = http_method
-        self._workload = self.get_workload()
-        self._success_counter = Value("i", 0)  # Actually, those who did not timed out
-        self._counter = 0
-    
-    def get_workload(self) -> List[int]:
-        raise NotImplementedError
+        self.__workload = (rate for rate in workload)
+        self.__counter = 0
+        self.kwargs = kwargs
     
     def start(self):
         total_seconds = 0
-        for rate in self._workload:
+        for rate in self.__workload:
             total_seconds += 1
-            self._counter += rate
-            generator_process = Process(
-                target=self.target_process, args=(
-                    rate, self._success_counter))
+            self.__counter += rate
+            generator_process = Process(target=self.target_process, args=(rate,))
             generator_process.daemon = True
             generator_process.start()
             active_children()
@@ -37,150 +31,105 @@ class BarAzmoon:
         
         print(f"total seconds: {total_seconds}")
 
-        return (self._counter, self._counter - self._success_counter.value)
+        return self.__counter, total_seconds
 
-    def target_process(self, count, success_counter):
-        asyncio.run(self.generate_load_for_second(count, success_counter))
+    def target_process(self, count):
+        asyncio.run(self.generate_load_for_second(count))
 
-    async def generate_load_for_second(self, count, success_counter):
+    async def generate_load_for_second(self, count):
         async with ClientSession() as session:
-            delays = np.cumsum(
-                np.random.exponential(1 / (count * 1.5), count))
+            delays = np.cumsum(np.random.exponential(1 / (count * 1.5), count))
             tasks = []
             for i in range(count):
-                task = asyncio.ensure_future(
-                    self.predict(delays[i], session, success_counter))
+                task = asyncio.ensure_future(self.predict(delays[i], session))
                 tasks.append(task)
             await asyncio.gather(*tasks)
     
-    async def predict(self, delay, session, success_counter):
+    async def predict(self, delay, session):
         await asyncio.sleep(delay)
         data_id, data = self.get_request_data()
-        async with getattr(
-            session, self.http_method)(
-                self.endpoint, data=data) as response:
+        async with getattr(session, self.http_method)(self.endpoint, data=data) as response:
             response = await response.json()
-            if "error" not in response.keys():
-                loop = asyncio.get_event_loop()
-                await loop.run_in_executor(
-                    None, self.increment_value, success_counter)
             self.process_response(data_id, response)
             return 1
     
-    def increment_value(self, value):
-        value.value += 1
-
     def get_request_data(self) -> Tuple[str, str]:
         return None, None
     
     def process_response(self, data_id: str, response: dict):
-        """
-        response fetch logic
-        e.g. prining
-        """
         pass
 
-# class SeldonBarAzmoon(BarAzmoon):
-#     def set_seldon_params(
-#         self, gateway_endpoint, deployment_name, namespace):
-#         self.sc = SeldonClient(
-#             gateway_endpoint=gateway_endpoint,
-#             gateway="istio",
-#             transport="rest",
-#             deployment_name=deployment_name,
-#             namespace=namespace)
-
-#     async def predict(self, delay, session, success_counter):
-#         await asyncio.sleep(delay)
-#         data_id, data = self.get_request_data()
-#         async with self.sc.predict(inputs = data) as response:
-#             if response.success:
-#                 json_data_timer = response.response['jsonData']['time']
-#                 return 1
 
 class MLServerBarAzmoon(BarAzmoon):
-    def __init__(
-        self, endpoint: str, http_method: str,
-        workload: Iterable, data: List,
-        data_shape: List, data_type: str):
-        self._workload = workload
-        self.data = data
-        self.data_shape = data_shape
-        self.data_type = data_type
-        super().__init__(endpoint, http_method)
-
-    def get_workload(self) -> List[int]:
-        return self._workload
-
-    async def predict(self, delay, session, success_counter):
+    async def predict(self, delay, session):
         await asyncio.sleep(delay)
-        data = self.get_request_data()
-
-        async with getattr(
-            session, self.http_method)(
-                self.endpoint, json=data) as response:
+        data_id, data = self.get_request_data()
+        async with getattr(session, self.http_method)(self.endpoint, json=data) as response:
+            print(await response.text())
+            print("*"*50)
             response = await response.json()
-            if "error" not in response.keys():
-                loop = asyncio.get_event_loop()
-                await loop.run_in_executor(
-                    None, self.increment_value, success_counter)
+            print("*"*50)
+            self.process_response(data_id, response)
             return 1
-    
+
     def get_request_data(self) -> Tuple[str, str]:
-        if self.data_type == 'example':
+        if self.kwargs['data_type'] == 'example':
             payload = {
                 "inputs": [
                     {
                         "name": "parameters-np",
                         "datatype": "FP32",
-                        "shape": self.data_shape,
-                        "data": self.data,
+                        "shape": self.kwargs['data_shape'],
+                        "data": self.kwargs['data'],
                         "parameters": {
                             "content_type": "np"
                         }
                     }]
                 }
-        elif self.data_type == 'audio':
+        elif self.kwargs['data_type'] == 'audio':
             payload = {
                 "inputs": [
                     {
                     "name": "array_inputs",
-                    "shape": self.data_shape,
+                    "shape": self.kwargs['data_shape'],
                     "datatype": "FP32",
-                    "data": self.data,
+                    "data": self.kwargs['data'],
                     "parameters": {
                         "content_type": "np"
                     }
                     }
                 ]
             }
-        elif self.data_type == 'text':
+        elif self.kwargs['data_type'] == 'text':
             payload = {
                 "inputs": [
                     {
                         "name": "text_inputs",
                         "shape": [1],
                         "datatype": "BYTES",
-                        "data": self.data,
+                        "data": self.kwargs['data'],
                         "parameters": {
                             "content_type": "str"
                         }
                     }
                 ]
             }
-        elif self.data_type == 'image':
+        elif self.kwargs['data_type'] == 'image':
             payload = {
                 "inputs":[
                     {
                         "name": "parameters-np",
                         "datatype": "INT32",
-                        "shape": self.data_shape,
-                        "data": self.data,
+                        "shape": self.kwargs['data_shape'],
+                        "data": self.kwargs['data'],
                         "parameters": {
                             "content_type": "np"
                             }
                     }]
                 }
         else:
-            raise ValueError(f'Unkown datatype {self.data_type}')
-        return payload
+            raise ValueError(f"Unkown datatype {self.kwargs['data_type']}")
+        return None, payload
+
+    def process_response(self, data_id: str, response: dict):
+        print(response)
