d0dee1b286918b578ec4f6d2a06e07f33e87113a
mehran <mehransi95@gmail.com>
2022-10-13 19:54:46 +0330

    Restructure the code

--- README.md
+++ README.md
@@ -1,32 +1,25 @@
-Web Service Load Tester
--
+# Web Service Load Tester
 
-
+> REST-based load tester implemented using asyncio and multiprocessing
 
 ### Usage
+---
+
 ```python
 import json
 from barazmoon import BarAzmoon
 
 
-
 class MyLoadTester(BarAzmoon):
-    endpoint="http://IP:PORT/PATH"
-    http_method="post"
-    
-    def get_workload(self):
-        return [7, 12, 0, 31, ...]  # each item of the list is the number of request for a second
-        ...
-    
-    @classmethod
-    def get_request_data(cls) -> Tuple[str, str]:
+    def get_request_data(self) -> Tuple[str, str]:
         return sending_data_id, json.dumps({"KEY": "VALUE"})
 
-    @classmethod
-    def process_response(cls, sent_data_id: str, response: json):
+    def process_response(self, sent_data_id: str, response: json):
         value = response.get("KEY")
         do_sth(value, sent_data_id)
 
-tester = MyLoadTester(...)
+
+workload = [7, 12, 0, 31, ...]  # each item of the list is the number of request for a second
+tester = MyLoadTester(workload=workload, endpoint="http://IP:PORT/PATH", http_method="post")
 tester.start()
 ```
--- barazmoon/main.py
+++ barazmoon/main.py
@@ -1,29 +1,25 @@
 import time
 from typing import List, Tuple
 import numpy as np
-from multiprocessing import Process, Value, active_children
+from multiprocessing import Process, active_children
 import asyncio
 from aiohttp import ClientSession
 
 
 class BarAzmoon:
-    endpoint: str = None
-    # timeout: int = None  # timeout: seconds to wait for server to respond to request, ignore when timed out
-    http_method = "get"
-    def __init__(self):
-        self.__workload = self.get_workload()
-        self.__success_counter = Value("i", 0)  # Actually, those who did not timed out
+    def __init__(self, *, workload: List[int], endpoint: str, http_method = "get", **kwargs):
+        self.endpoint = endpoint
+        self.http_method = http_method
+        self.__workload = (rate for rate in workload)
         self.__counter = 0
-    
-    def get_workload(self) -> List[int]:
-        raise NotImplementedError
+        self.kwargs = kwargs
     
     def start(self):
         total_seconds = 0
         for rate in self.__workload:
             total_seconds += 1
             self.__counter += rate
-            generator_process = Process(target=self.target_process, args=(rate, self.__success_counter))
+            generator_process = Process(target=self.target_process, args=(rate,))
             generator_process.daemon = True
             generator_process.start()
             active_children()
@@ -34,42 +30,30 @@ class BarAzmoon:
         
         print(f"total seconds: {total_seconds}")
 
-        return (self.__counter, self.__counter - self.__success_counter.value)
+        return self.__counter, total_seconds
 
-    @classmethod
-    def target_process(cls, count, success_counter):
-        asyncio.run(cls.generate_load_for_second(count, success_counter))
+    def target_process(self, count):
+        asyncio.run(self.generate_load_for_second(count))
 
-    @classmethod
-    async def generate_load_for_second(cls, count, success_counter):
+    async def generate_load_for_second(self, count):
         async with ClientSession() as session:
             delays = np.cumsum(np.random.exponential(1 / (count * 1.5), count))
             tasks = []
             for i in range(count):
-                task = asyncio.ensure_future(cls.predict(delays[i], session, success_counter))
+                task = asyncio.ensure_future(self.predict(delays[i], session))
                 tasks.append(task)
             await asyncio.gather(*tasks)
     
-    @classmethod
-    async def predict(cls, delay, session, success_counter):
+    async def predict(self, delay, session):
         await asyncio.sleep(delay)
-        data_id, data = cls.get_request_data()
-        async with getattr(session, cls.http_method)(cls.endpoint, data=data) as response:
+        data_id, data = self.get_request_data()
+        async with getattr(session, self.http_method)(self.endpoint, data=data) as response:
             response = await response.json()
-            if "error" not in response.keys():
-                loop = asyncio.get_event_loop()
-                await loop.run_in_executor(None, cls.increment_value, success_counter)
-            cls.process_response(data_id, response)
+            self.process_response(data_id, response)
             return 1
     
-    @staticmethod
-    def increment_value(value):
-        value.value += 1
-
-    @classmethod
-    def get_request_data(cls) -> Tuple[str, str]:
+    def get_request_data(self) -> Tuple[str, str]:
         return None, None
     
-    @classmethod
-    def process_response(cls, data_id: str, response: dict):
+    def process_response(self, data_id: str, response: dict):
         pass
