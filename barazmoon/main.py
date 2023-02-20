f32560bb8aa8d9b15432d84d60b5e419dbb116e2
mehran <mehransi95@gmail.com>
2023-02-20 22:52:15 +0330

    Add counter for received responses

--- barazmoon/main.py
+++ barazmoon/main.py
@@ -1,7 +1,7 @@
 import time
 from typing import List, Tuple
 import numpy as np
-from multiprocessing import Process, active_children
+from multiprocessing import Process, active_children, Value
 import asyncio
 from aiohttp import ClientSession, TCPConnector
 
@@ -14,13 +14,14 @@ class BarAzmoon:
         self.__workload = (rate for rate in workload)
         self.__counter = 0
         self.kwargs = kwargs
+        self.__success_counter = Value('i', 0)
     
     def start(self):
         total_seconds = 0
         for rate in self.__workload:
             total_seconds += 1
             self.__counter += rate
-            generator_process = Process(target=self.target_process, args=(rate,))
+            generator_process = Process(target=self.target_process, args=(rate, self.__success_counter))
             generator_process.daemon = True
             generator_process.start()
             active_children()
@@ -31,27 +32,34 @@ class BarAzmoon:
         
         print(f"total seconds: {total_seconds}")
 
-        return self.__counter, total_seconds
+        return self.__counter, self.__success_counter.value
 
-    def target_process(self, count):
-        asyncio.run(self.generate_load_for_second(count))
+    def target_process(self, count, success_counter: Value):
+        count = asyncio.run(self.generate_load_for_second(count))
+        with success_counter.get_lock():
+            success_counter.value += count
 
     async def generate_load_for_second(self, count):
         async with ClientSession(connector=TCPConnector(limit=0)) as session:
             delays = np.cumsum(np.random.exponential(1 / (count * 1.5), count))
             tasks = []
             for i in range(count):
-                task = asyncio.ensure_future(self.predict(delays[i], session))
+                task = asyncio.create_task(self.predict(delays[i], session))
                 tasks.append(task)
-            await asyncio.gather(*tasks)
+            results = await asyncio.gather(*tasks)
+            return sum(results)
     
     async def predict(self, delay, session):
         await asyncio.sleep(delay)
         data_id, data = self.get_request_data()
-        async with getattr(session, self.http_method)(self.endpoint, data=data) as response:
-            response = await response.json(content_type=None)
-            self.process_response(data_id, response)
-            return 1
+        try:
+            async with getattr(session, self.http_method)(self.endpoint, data=data) as response:
+                response = await response.json(content_type=None)
+                self.process_response(data_id, response)
+                return 1
+        except Exception as exc:
+            print(exc)
+            return 0
     
     def get_request_data(self) -> Tuple[str, str]:
         return None, None
