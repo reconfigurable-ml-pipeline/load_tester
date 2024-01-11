00210a64273b5c7d0fa165f0eea944a93db2f625
Saeid Ghafouri <s.ghafouri@qmul.ac.uk>
2024-01-11 17:32:51 +0000

    successfully passing the first request through

--- barazmoon/main.py
+++ barazmoon/main.py
@@ -180,7 +180,9 @@ async def request_after_grpc(stub, metadata, wait, payload, ignore_output=False)
         resp["times"] = times
         resp["model_name"] = grpc_resp.model_name
         if hasattr(inference_response.outputs[0].parameters, 'times'): # handling drops
-            times["models"] = eval(eval(inference_response.outputs[0].parameters.times)[0])
+            # times["models"] = eval(eval(inference_response.outputs[0].parameters.times)[0])
+            # times["models"] = eval(inference_response.outputs[0].parameters.times)
+            times["models"] = inference_response.outputs[0].parameters.times
             # times["models"] = eval(inference_response.outputs[0].parameters.times)
             resp["outputs"] = [outputs]
         else:
